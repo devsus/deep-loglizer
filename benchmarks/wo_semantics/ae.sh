fed6cfc3d629729b47f343ab6ea158dd33a465fc
devsus <suha2200@student.miun.se>
2025-11-23 16:59:08 +0100

    removed redundant flags

--- benchmarks/wo_semantics/ae.sh
+++ benchmarks/wo_semantics/ae.sh
@@ -1,3 +1,3 @@
 python ae_demo.py --feature_type sequentials --anomaly_ratio 0.03 --dataset HDFS --data_dir ../data/processed/HDFS/hdfs_0.0_tar
 python ae_demo.py --feature_type sequentials --anomaly_ratio 0.8 --dataset BGL --data_dir ../data/processed/BGL/bgl_0.0_tar
-torchrun --nproc-per-node=gpu ae_demo.py --feature_type sequentials --dataset HDFS --data_dir ../data/processed/HDFS_100k/hdfs_0.0_tar --anomaly_ratio 0.1 --cache
+torchrun --nproc-per-node=gpu ae_demo.py --feature_type sequentials --dataset HDFS --data_dir ../data/processed/HDFS_100k/hdfs_0.0_tar --anomaly_ratio 0.1
