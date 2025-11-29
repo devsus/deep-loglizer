0dad40abd033717cd29c32ed8c127810e8ed0ab6
devsus <susannahasselrot@gmail.com>
2025-11-29 18:23:53 +0100

    removed obsolete parsers

--- verify_params.py
+++ verify_params.py
@@ -1,10 +1,10 @@
 import torch.nn as nn
 
 lstm = nn.LSTM(
-            input_size=32,  # embedding dim
-            hidden_size=128,
+            input_size=512,  # embedding dim
+            hidden_size=2048,
             batch_first=True,
-            num_layers=2,
+            num_layers=32,
             bidirectional=True,     # if dir=2
         )
 
@@ -12,11 +12,11 @@ total = sum(p.numel() for p in lstm.parameters())
 print(total)
 
 encoder_layer = nn.TransformerEncoderLayer(
-    d_model=32,             # embedding_dim
-    nhead=2,                # heads scaled for hidden size
-    dim_feedforward=128,    # hidden size
+    d_model=512,             # embedding_dim
+    nhead=32,                # heads scaled for hidden size
+    dim_feedforward=2048,    # hidden size
     batch_first=True
 )
-transformer = nn.TransformerEncoder(encoder_layer, num_layers=2)
+transformer = nn.TransformerEncoder(encoder_layer, num_layers=32)
 total = sum(p.numel() for p in transformer.parameters())
 print(total)
